30711b60ac659b7bb8bb17994b927847d18ebd01
matejscerba <m.scerba@seznam.cz>
2021-04-28 16:31:27 +0100

    detection ok

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/matejscerba/Developer/cpp/rp
-# Build directory: /Users/matejscerba/Developer/cpp/rp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
