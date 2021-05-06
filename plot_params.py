28d72e495cc7dba8ea1eb75f3196e702c8829104
matejscerba <m.scerba@seznam.cz>
2021-05-06 09:09:17 +0100

    basic plotting done

--- plot_params.py
+++ plot_params.py
@@ -30,34 +30,48 @@ def read_file(path):
     return video, names, values
 
 def get_valid_values(values):
-    return None
+    res = []
+    for x in values:
+        if x == None:
+            break
+        res.append(x)
+    return res
+
+def none_to_zero(values):
+    return [0 if x == None else x for x in values]
 
 def plot_bars(x, y, x_name, y_name):
-    pass
+    plt.bar(x, y)
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
 
 def plot_lines(x, y, x_name, y_name):
-    pass
+    plt.plot(x, y)
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
+    
 
 def plot_params(vid_name, names, vals):
     for p_name, p_vals in zip(names[1:], vals[1:]):
         plt.title("{} - {}".format(vid_name, p_name))
         if p_name == "Hips velocity loss" or p_name == "Shoulders velocity loss" or p_name == "Takeoff angle":
+            # Don't plot this parameter.
             continue
         elif "Step" in p_name or "step" in p_name:
-            # Bar chart.
+            # Plot as bar chart.
             valid_vals = get_valid_values(p_vals)
-            plot_bars(range(len(valid_vals)), valid_vals, "Step", p_name)
+            plot_bars(range(1, len(valid_vals) + 1), valid_vals, "Step", p_name)
             pass
         elif "tilt" in p_name:
-            # Frame-wise bar chart.
-            plot_bars(vals[0], p_vals, names[0], p_name)
+            # Plot as frame-wise bar chart.
+            valid_vals = none_to_zero(p_vals)
+            plot_bars(vals[0], valid_vals, names[0], p_name)
             pass
         else:
-            # Frame-wise line segments chart.
+            # Plot as frame-wise line segments chart.
             plot_lines(vals[0], p_vals, names[0], p_name)
             pass
         plt.show()
-    pass
 
 if args.file != None:
     data = read_file(args.file)
